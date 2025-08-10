d859464a7d15dc582f46b995bd8bd8f8dd460b29
Well <wevelle98@gmail.com>
2025-08-10 00:21:39 -0300

    feat: create user entity with getters, setters and constructors

--- src/main/java/com/espro/falatudopodcast/users/userModel.java
+++ src/main/java/com/espro/falatudopodcast/users/userModel.java
@@ -1,6 +1,95 @@
 package com.espro.falatudopodcast.users;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users_table")
+
+
 public class userModel {
+    public userModel() {
+    }
+
+    public userModel(int id,String name, String username, String password, String email, int idade, String urlProfilePicture) {
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.idade = idade;
+        this.urlProfilePicture = urlProfilePicture;
+    }
 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Column(name="username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
+    @Column(name="idade")
+    private int idade;
+    @Column(name="url_profile_picture")
+    private String urlProfilePicture;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getUrlProfilePicture() {
+        return urlProfilePicture;
+    }
+
+    public void setUrlProfilePicture(String urlProfilePicture) {
+        this.urlProfilePicture = urlProfilePicture;
+    }
 }
