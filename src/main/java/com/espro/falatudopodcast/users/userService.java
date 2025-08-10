6711619da4fc2413bc2e729a3331cb6208bd04c4
Well <wevelle98@gmail.com>
2025-08-10 00:34:49 -0300

    feat: add Repository and Service layers for User entity

--- src/main/java/com/espro/falatudopodcast/users/userService.java
+++ src/main/java/com/espro/falatudopodcast/users/userService.java
@@ -1,4 +1,9 @@
 package com.espro.falatudopodcast.users;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class userService {
+        private userRepository userRepository;
+
 }
