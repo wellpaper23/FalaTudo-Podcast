753108905f339fe7a12a764579bdf5235da9201e
Well <wevelle98@gmail.com>
2025-08-11 16:09:31 -0300

    feat: implement CRUD endpoints in UserController for user management

--- src/main/java/com/espro/falatudopodcast/users/userService.java
+++ src/main/java/com/espro/falatudopodcast/users/userService.java
@@ -5,5 +5,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class userService {
         private userRepository userRepository;
+        public userService(userRepository userRepository) {
+                this.userRepository = userRepository;
+        }
+
+        //criar usuarios
+
+        //ver usuarios
+
+        //alterar usuários
+
+        //deletar usuários
+
 
 }
--- src/main/java/com/espro/falatudopodcast/users/usersController.java
+++ src/main/java/com/espro/falatudopodcast/users/usersController.java
@@ -1,8 +1,7 @@
 package com.espro.falatudopodcast.users;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.service.annotation.DeleteExchange;
 
 @RestController
 @RequestMapping("/user")
@@ -10,8 +9,32 @@ public class usersController {
 
 
     @GetMapping()
-    public String user() {
-        return "esta é a pagina para gerenciar os usuarios, somente administradores devem ter acesso";
+    public String userPage() {
+        return "esta é a pagina para gerenciar os usuarios, somente administradores devem ter acesso</h1>";
     }
-
+    //criar usuario
+    @PostMapping("/create")
+    public String createUser() {
+        return "usuario criado com sucesso!";
+    }
+    //ver todos usuario
+    @GetMapping("list")
+    public String listUsers() {
+        return "usuarios listados com sucesso!";
+    }
+    //ver usuario especifico
+    @GetMapping("/{id}")
+    public String showUser() {
+        return "usuario exibido com sucesso!";
+    }
+    //editar usuario
+    @PutMapping("/updateUser")
+    public String updateUser() {
+        return "usuario atualizado com sucesso!";
+    }
+    //deletar usuario
+    @DeleteMapping("/delete")
+    public String deleteUser() {
+        return  "usuario deletado com sucesso!";
+    }
 }
