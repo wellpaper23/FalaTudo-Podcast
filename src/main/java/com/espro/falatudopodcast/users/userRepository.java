de4972d1dfc06eb6e0437f211ad108ac45cef521
Well <wevelle98@gmail.com>
2025-08-11 23:24:53 -0300

    chore: add @Repository annotation to userRepository Interface

--- src/main/java/com/espro/falatudopodcast/users/userRepository.java
+++ src/main/java/com/espro/falatudopodcast/users/userRepository.java
@@ -1,7 +1,9 @@
 package com.espro.falatudopodcast.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface userRepository extends JpaRepository<userModel,Long> {
 
 }
